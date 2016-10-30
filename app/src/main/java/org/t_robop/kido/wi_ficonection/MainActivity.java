61e75c19def08107e91ee0fcb1c63a80fff7bca2
kaikoro <kaikoro.pk@gmail.com>
2016-10-30 11:16:33 +0900

    IDとPasswordの保存を実装

--- app/src/main/java/org/t_robop/kido/wi_ficonection/MainActivity.java
+++ app/src/main/java/org/t_robop/kido/wi_ficonection/MainActivity.java
@@ -1,7 +1,10 @@
 package org.t_robop.kido.wi_ficonection;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //IDとPasswordの保存
+    public void save(View v){
+        EditText Id =(EditText) findViewById(R.id.id);
+        EditText password =(EditText) findViewById(R.id.password);
+        //保存
+        SharedPreferences data = getSharedPreferences("IdInfo",MODE_PRIVATE);
+        SharedPreferences.Editor editor = data.edit();
+        editor.putString("ID", Id.getText().toString());
+        editor.putString("password",password.getText().toString());
+        editor.apply();
     }
 }
