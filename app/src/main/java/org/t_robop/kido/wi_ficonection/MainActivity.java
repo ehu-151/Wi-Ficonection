8f85fdcbabd5a5fccf88f26b1e32e2721e56640f
kaikoro <kaikoro.pk@gmail.com>
2016-10-30 11:42:36 +0900

    アプリ起動時、IDとPasswordの読み込みを実装

--- app/src/main/java/org/t_robop/kido/wi_ficonection/MainActivity.java
+++ app/src/main/java/org/t_robop/kido/wi_ficonection/MainActivity.java
@@ -7,22 +7,32 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //EditTextの宣言
+    EditText id;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //EditTextの関連付け
+        id =(EditText) findViewById(R.id.id);
+        password =(EditText) findViewById(R.id.password);
+
+        SharedPreferences data = getSharedPreferences("IdInfo", MODE_PRIVATE);
+        id.setText(data.getString("id",""));
+        password.setText(data.getString("password",""));
     }
 
     //IDとPasswordの保存
     public void save(View v){
-        EditText Id =(EditText) findViewById(R.id.id);
-        EditText password =(EditText) findViewById(R.id.password);
         //保存
         SharedPreferences data = getSharedPreferences("IdInfo",MODE_PRIVATE);
         SharedPreferences.Editor editor = data.edit();
-        editor.putString("ID", Id.getText().toString());
-        editor.putString("password",password.getText().toString());
+        editor.putString("id", id.getText().toString());
+        editor.putString("password", password.getText().toString());
         editor.apply();
     }
 }
