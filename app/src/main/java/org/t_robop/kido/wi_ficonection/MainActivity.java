9c056c3891c27739fb340d5626783e721aafd7a1
kaikoro <kaikoro.pk@gmail.com>
2016-10-31 00:08:31 +0900

    Wi-FiのON OFFを確認

--- app/src/main/java/org/t_robop/kido/wi_ficonection/MainActivity.java
+++ app/src/main/java/org/t_robop/kido/wi_ficonection/MainActivity.java
@@ -1,10 +1,12 @@
 package org.t_robop.kido.wi_ficonection;
 
 import android.content.SharedPreferences;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //EditTextの宣言
@@ -34,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("id", id.getText().toString());
         editor.putString("password", password.getText().toString());
         editor.apply();
+    }
+    public void connect(View v){
+        final WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        //Wi-FiのON OFFを確認
+        if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
+            //ONの時
+        }else {
+            //OFFの時
+            Toast.makeText(this, "Wi-FiをONにしてください", Toast.LENGTH_SHORT).show();
+        }
     }
 }
