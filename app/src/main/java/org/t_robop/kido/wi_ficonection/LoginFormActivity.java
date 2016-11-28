3483d7ea2ba5f792f102082bf05a02d1f57a351b
kaikoro <kaikoro.pk@gmail.com>
2016-11-28 20:32:36 +0900

    jsoup用のクラス作成

--- app/src/main/java/org/t_robop/kido/wi_ficonection/LoginFormActivity.java
+++ app/src/main/java/org/t_robop/kido/wi_ficonection/LoginFormActivity.java
@@ -1,11 +1,7 @@
 package org.t_robop.kido.wi_ficonection;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginFormActivity extends AppCompatActivity {
 
@@ -13,40 +9,6 @@ public class LoginFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
-
-        //レイアウトで指定したWebViewのIDを指定する。
-        WebView myWebView = (WebView)findViewById(R.id.webView1);
-
-        //リンクをタップしたときに標準ブラウザを起動させない
-        myWebView.setWebViewClient(new WebViewClient());
-
-        //最初にgoogleのページを表示する。
-        myWebView.loadUrl("http://www.apple.com/jp//?afid=p238%7CZUkI5bsT-dc_mtid_18707vxu38484&cid=aos-jp-kwyh-brand");
-
-        //jacascriptを許可する
-        myWebView.getSettings().setJavaScriptEnabled(true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
 
